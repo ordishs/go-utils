17269afae254d06a4c9c6dabcaba32cc2d55d159
Simon Ordish <simon.ordish@masagi.co.uk>
2022-12-06 14:05:04 +0100

    Add GetBitcoinHash

--- Sha256d.go
+++ Sha256d.go
@@ -8,3 +8,12 @@ func Sha256d(b []byte) []byte {
 	second := sha256.Sum256(first[:])
 	return second[:]
 }
+
+// GetBitcoinHash calculates the Bitcoin hash of the given bytes.
+// The Bitcoin hash is the double SHA-256 hash of the given bytes
+// which is then reversed.
+// For transactions the entire transaction bytes are hashed; for blocks
+// only the header (80 bytes) is hashed.
+func GetBitcoinHash(b []byte) []byte {
+	return ReverseSlice(Sha256d(b))
+}
