1968366b935a555a4ac4db2295675eb52a29f010
Simon Ordish <simon.ordish@masagi.co.uk>
2023-02-12 13:57:47 +0100

    Extra indent

--- stat/AtomicStats.go
+++ stat/AtomicStats.go
@@ -98,7 +98,7 @@ func (s *AtomicStats) String(indent string) string {
 		if sb.Len() > 0 {
 			sb.WriteString(indent)
 		}
-		sb.WriteString(fmt.Sprintf("%15s: %5d (%s)\n", key, stat.count, stat.GetAverageString()))
+		sb.WriteString(fmt.Sprintf("%20s: %5d (%s)\n", key, stat.count, stat.GetAverageString()))
 	}
 
 	avgDuration := totalDuration / time.Duration(totalCount)
